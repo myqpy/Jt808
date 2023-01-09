fb5dc07778cd8ee7a89b9ce99a3df9644355f606
myqpy <qupengyu1@gmail.com>
2023-01-09 16:06:17 +0800

    2023.1.9
    v1.1 finale

--- User/main.c
+++ User/main.c
@@ -69,8 +69,8 @@ int main(void)
 		time_1s = 0;
 		initSystemParameters(1); //0 烧写出厂参数 1 不烧写出厂参数
 		//设置手机号（唯一识别id）
-		setTerminalPhoneNumber("100221000011", 12);
-		setTerminalId("1000011", 8);
+		setTerminalPhoneNumber("100221000210", 12);
+		setTerminalId("1000210", 8);
 		//连接服务器
 		if(isTCPconnected == 0)
 		{
